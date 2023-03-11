082c6f35f5d93a17d55dc62d1b4edc4ea5c5e3e0
Limber <limberbazan@gmail.com>
2023-03-10 21:36:16 -0500

    ejercicio2 done

---
+++ section2/ejercicio2.js
@@ -0,0 +1,9 @@
+const sobre_mi = {
+    nombre: "kfasso",
+    edad: 20,
+    desarrollador: true,
+    FNacimiento: new Date (2003,4,18),
+    libroFavorito: "Los Inocentes"
+}
+
+console.log(sobre_mi.FNacimiento);
