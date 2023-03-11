9b8be25700d65db63751a0c4840befc6b166fe9b
Limber <limberbazan@gmail.com>
2023-03-10 21:47:04 -0500

    ejercicio2 fixed

--- section2/ejercicio2.js
+++ section2/ejercicio2.js
@@ -1,9 +1,10 @@
-const sobre_mi = {
-    nombre: "kfasso",
-    edad: 20,
-    desarrollador: true,
-    FNacimiento: new Date (2003,4,18),
-    libroFavorito: "Los Inocentes"
+const libroFavorito = {
+    titulo: "Los Inocentes",
+    autor: "Oswaldo Reynoso",
+    fecha: new Date(1961),
+    url: "https://www.amazon.com/-/es/dp/B08T6Q4VWC/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2BL6AXS2VOEMV&keywords=los+inocentes+oswaldo&qid=1678502533&sprefix=los+inocentes+oswaldo%2Caps%2C163&sr=8-1"
 }
 
-console.log(sobre_mi.FNacimiento);
+const myList = ["kfasso", 20, true, new Date(2023,4,18), libroFavorito.titulo, libroFavorito.autor, libroFavorito.fecha, libroFavorito.url]
+
+console.log(myList);
