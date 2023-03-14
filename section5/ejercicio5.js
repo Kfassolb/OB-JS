191ba8d1b95984c4b39a30599fce8c2c24dc0092
Limber <limberbazan@gmail.com>
2023-03-13 19:58:08 -0500

    ejercicio5 done

---
+++ section5/ejercicio5.js
@@ -0,0 +1,11 @@
+let alturaCm = 175;
+let alturaM = 1.75;
+let pesoKg = 65.13;
+let alturaRedondeado = alturaM.toPrecision(1);
+let pesoRedondeado = pesoKg.toPrecision(2);
+
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+
+console.log(sonIguales);
+
---
+++ section5/numbers.js
@@ -0,0 +1,7 @@
+let min_precision = Number.EPSILON;
+let min_valor_js = Number.MIN_VALUE;
+let max_valor_js =  Number.MAX_VALUE
+
+console.log(min_precision);
+console.log(min_valor_js);
+console.log(max_valor_js);
